71fc0f5f67524d3599b44f258aedc83081639067
Veronica RG <vero.rg0416@gmail.com>
2022-09-07 18:10:58 -0300

    feat: add font

--- App.tsx
+++ App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 
+import {
+  useFonts,
+  Poppins_400Regular, 
+  Poppins_500Medium,
+  Poppins_700Bold
+} from '@expo-google-fonts/poppins'
 import theme from './src/global/styles/theme'
+
 import Home from './src/screens/Home';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
+  const [fontsLoaded]= useFonts({ 
+    Poppins_400Regular, 
+    Poppins_500Medium,
+    Poppins_700Bold});
+  
+  !fontsLoaded && <AppLoading/>
+
   return (
     <ThemeProvider theme={theme}>
       <Home/>
